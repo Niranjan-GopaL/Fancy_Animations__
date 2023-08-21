496ecfa6c06eda123b2b293be7a202241cf00b28
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-08-22 01:36:36 +0530

    We need useEffect hooks to make animations work, motion dev tools is also installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { animate } from "motion"
+import { useEffect } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+
+    useEffect(() => {
+        animate(
+            "#box",
+            { rotate: 90 },
+            {
+                duration: 0.5,
+                easing: "ease-in-out",
+                repeat: 3,
+                direction: "alternate"
+            }
+        );
+    }, []); // The empty dependency array ensures the effect runs once after initial render
+
+
+
+    return (
+        <>
+            <div id="box" style={{height: "100px", width: "100px",background: "green"}}  />            
+        </>
+    )
 }
+
+
 
 export default App
